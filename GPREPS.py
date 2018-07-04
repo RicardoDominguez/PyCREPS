92995b9aa665860628e6f1ace324b9f2c6f273b1
RicardoDominguez <riddoddo@gmail.com>
2018-07-04 13:37:21 +0100

    Quickly converges to 1

--- GPREPS.py
+++ GPREPS.py
@@ -7,42 +7,46 @@ from highpol        import HighPol
 from policy         import TilePol
 from predictReward  import predictReward
 from plant          import Plant
+import matplotlib.pyplot as plt
 
 # Indexes for the state variables being fed to GP, policy, cost function
-dyni = [1, 3]      # GP inputs
-dyno = [0, 1, 2]   # GP outputs
-difi = [0, 1, 2]   # Variables trained by differences
+dyni = [1]      # GP inputs
+dyno = [0]     # GP outputs
+difi = [0]   # Variables trained by differences
 ipol = [0]         # Policy inputs
-icos = [2]         # Cost function inputs
+icos = [0]         # Cost function inputs
+nstates = 2
 
 # Algorithm parameters
-eps = 3            # Relative entropy bound (lower -> more exploration)
-K = 3              # Number of policy iterations
+eps = 1            # Relative entropy bound (lower -> more exploration)
+K = 10              # Number of policy iterations
 M = 10000          # Number of simulated rollouts
 NinitRolls = 1     # Number of initial rollouts
 
 # Simulated episode parameters
-x0 = [0, 0, 0] # Initial state
-H = 50 # Simulation horizon
+x0 = np.array([0]) # Initial state
+H = 10 # Simulation horizon
 
 # Low level policy
-nX = H
-deltaX = 1
+nX = 10
+deltaX = 2
 minU = 0
-maxU = 4200
+maxU = 4
 pol = TilePol(nX, deltaX, minU, maxU)
 
 # High level policy
-W_mean = 2700
-W_dev = W_mean / 10
+W_mean = 2
+W_dev = W_mean / 1.0
 hpol_mu = np.random.normal(W_mean, W_dev, nX);
-hpol_sigma = np.eye(nX) * 4000
+print '-------------------------------------------'
+print 'Initial policy weights', hpol_mu
+hpol_sigma = np.eye(nX) * np.power(W_dev, 2)
 hpol = HighPol(hpol_mu, hpol_sigma)
 
 # Cost function
-K = 20
+Kcost = 50
 target = 0
-cost = CostFcn(K, icos, target)
+cost = CostFcn(Kcost, icos, target)
 
 # Forward model
 gp = GPS(dyni, dyno, difi)
@@ -50,18 +54,28 @@ gp = GPS(dyni, dyno, difi)
 # System being
 sys = Plant()
 
+rewards = []
+
 # Initial rollouts
-X, Y = np.empty([0, 0]), np.empty([0, 0])
+X, Y = np.empty([0, nstates]), np.empty([0, nstates])
 for j in xrange(NinitRolls):
-    x, y = sys.rollout(pol)
-    X = np.append(X, x)
-    Y = np.append(Y, y)
+    x, y = sys.rollout(hpol, pol)
+    X = np.concatenate((X, x))
+    Y = np.concatenate((Y, y))
 
 # Policy iteration
 for k in xrange(K):
+    print '--------------------------------------'
+    print 'Run', k+1, 'out of', K
     gp.fit(X, Y)
     R, W, F = predictReward(x0, M, H, hpol, pol, gp, cost)
-    hpol.update(R, W, F, eps)
-    x, y = sys.rollout(pol)
-    X = np.append(X, x)
-    Y = np.append(Y, y)
+    hpol.update(W, R, F, eps)
+    x, y = sys.rollout(hpol, pol)
+    X = np.concatenate((X, x))
+    print 'Rollout y', y
+    run_cost = cost.sample(y[:, 0].T)
+    rewards.append(run_cost[0,0])
+    print 'Rewards', rewards
+    Y = np.concatenate((Y, y))
+plt.plot(rewards)
+plt.show()
--- dual_fnc.py
+++ dual_fnc.py
@@ -1,91 +1,136 @@
 # GPREPS dual function minimizer
 import numpy as np
-from pyipm import IPM
+import math
+#from pyipm import IPM
+import scipy.optimize as opt
+import matplotlib.pyplot as plt
+from scipy.misc import logsumexp
+import pdb
 
-class DualFnc:
-    def updateArguments(self, W, R, F, eps):
-        '''
-        Inputs:
+def fnc(x, W, R, F, eps):
+    '''
+    Implementation of the GPREPS dual function
+
+    Inputs:
+        x           array containing (eta, theta)       (1 x 1+S)
+        args        tuple containing the following arguments
             w       Weight  dataset matrix  (N x W)
             R       Return  dataset vector  (N x 1)
             F       Feature dataset matrix  (N x S)
             eps     Epsilon                 (1 x 1)
-        '''
-        self.W = W
-        self.R = R
-        self.F = F
-        self.eps = eps
 
-    def updateEtaTheta(self, x):
-        '''
-        Changes:
-            eta         (1 x 1)
-            theta       (S x 1)
-        '''
-        self.eta = x[0]
-        self.theta = theta[1:]
+    Outputs:
+        fval    value of the dual function at x         (1 x 1)
+    '''
+    eta, theta = x[0], np.array(x[1:]).reshape(-1, 1)
 
-    def fnc(self, x, eval_grad = False):
-        '''
-        Implementation of the GPREPS dual function
+    # Evaluate dual function at x
+    N = W.shape[0]
+    mean_f = np.mean(F, 0).reshape(-1, 1)                  # (S x 1)
+    err = R - np.sum(theta.T * F, 1).reshape(-1, 1)   # (N x 1)
+    # zeta = np.exp(err / float(eta))                             # (N x 1)
+    # s_zeta = np.sum(zeta)
+    # summa = s_zeta / float(N)
+    # fval = (eta * np.log(summa)) + (eta * eps) + (np.dot(theta.T, mean_f))
+    log_summa = logsumexp(err / eta, b = 1.0 / N)
+    fval = (eta * log_summa) + (eta * eps) + (np.dot(theta.T, mean_f))
+    return fval
 
-        Inputs:
-            x           tuple containing (eta, theta)       (1 x 1+S)
-            eval_grad   if True the graident is evaluated
+def fnc_der(x, W, R, F, eps):
+    '''
+    Implementation of the derivative of the GPREPS dual function
 
-        Outputs:
-            fval    value of the dual function at x         (1 x 1)
-            grad    gradient of the dual function at x      (1 x 1+S)
-        '''
-        eta, theta = x[0], np.array(x[1:]).reshape(-1, 1)
+    Inputs:
+        x           array containing (eta, theta)       (1 x 1+S)
+        args        tuple containing the following arguments
+            w       Weight  dataset matrix  (N x W)
+            R       Return  dataset vector  (N x 1)
+            F       Feature dataset matrix  (N x S)
+            eps     Epsilon                 (1 x 1)
 
-        # Evaluate dual function at x
-        N = self.W.shape[0]
-        mean_f = np.mean(self.F, 0).reshape(-1, 1)                  # (S x 1)
-        err = self.R - np.sum(theta.T * self.F, 1).reshape(-1, 1)   # (N x 1)
-        zeta = np.exp(err / float(eta))                             # (N x 1)
-        s_zeta = np.sum(zeta)
-        summa = s_zeta / float(N)
-        fval = (eta * np.log(summa)) + (eta * self.eps) + (np.dot(theta.T, mean_f))
+    Outputs:
+        grad    gradient of the dual function at x      (1 x 1+S)
+    '''
+    eta, theta = x[0], np.array(x[1:]).reshape(-1, 1)
 
-        # Evaluate gradient
-        if eval_grad:
-            # Evaluate gradient w.r.t eta
-            base = np.sum(zeta * err) / (eta * np.sum(zeta)) # (1 x 1)
-            d_eta = self.eps + np.log(summa) - base
+    N = W.shape[0]
+    mean_f = np.mean(F, 0).reshape(-1, 1)                  # (S x 1)
 
-            # Evaluate gradient w.r.t. theta
-            d_theta = mean_f - (np.sum(zeta * self.F, 0).reshape(-1,1) / float(s_zeta)) # (S x 1)
-            grad = np.append(d_eta, d_theta).reshape(1, -1) # (1 x S + 1)
-            return fval, grad
-        return fval
+    err = R - np.sum(theta.T * F, 1).reshape(-1, 1)   # (N x 1)
+    # zeta = np.exp(err / float(eta))                             # (N x 1)
+    # s_zeta = np.sum(zeta)
+    # summa = s_zeta / float(N)
+    log_summa = logsumexp(err / eta, b = 1.0 / N)
 
-    def minimize(self):
+    # base = np.sum(zeta * err) / (eta * np.sum(zeta)) # (1 x 1)
+    base = np.exp(logsumexp(err / eta, b = err) - logsumexp(err / eta) - np.log(eta))
+    d_eta = eps + log_summa - base
+
+    #pdb.set_trace()
+    #d_theta = mean_f - (np.sum(zeta*F, 0).reshape(-1,1) / float(s_zeta)) # (S x 1)
+    d_theta = np.zeros(mean_f.shape)
+
+    grad = np.append(d_eta, d_theta).reshape(-1, 1) # (1 x S + 1)
+    return grad
+
+class DualFnc:
+    def minimize(self, W, R, F, eps):
         '''
         Minimize dual function with constrain using the interior point algorithm.
         '''
-        n_theta = self.F.shape[1]
+        n_theta = F.shape[1]
 
         # Ensure initial point is defined
-        #eta_undefined = True
-        eta0, theta0 = 1, np.zeros((1, n_theta))
-        x0 = np.append(eta0, theta0)
+        #min_eta = np.max(R) / np.log(1e300)
+        min_eta = 1e-299
+        eta0 = 1
+        # eta_undefined = True
+        # eta0, theta0 = 1e-12, np.zeros((1, n_theta))
+        # x0 = np.append(eta0, theta0)
+        # #print 'check undef'
         # while(eta_undefined):
         #     eta0 = 10 * eta0
         #     x0 = np.append(eta0, theta0)
-        #     val, grad = self.fcn(x0)
-        #     eta_undefined = math.isinf(val) or math.isnan(val) or math.isinf(grad(1)) or math.isnan(grad(1))
+        #     pdb.set_trace()
+        #     val = fnc(x0, W, R, F, eps)
+        #     #print eta0
+        #     #print val
+        #     if eta0 > 1e50:
+        #         break
+        #     eta_undefined = math.isinf(val) or math.isnan(val)
+        theta0 = np.zeros((1, n_theta))
+        x0 = np.append(eta0, theta0)
+        args = (W, R, F, eps)
+        eta_bds = (min_eta, None)
+        theta_bds = tuple([(None, None) for i in xrange(n_theta)])
+        bds = tuple([eta_bds]) + theta_bds
+        #problem = pyipm.IPM(x0=x0, x_dev=x_dev, f=f, ce=ce, ci=ci)
+        #x = problem.solve(x0=x0)
 
-        problem = pyipm.IPM(x0=x0, x_dev=x_dev, f=f, ce=ce, ci=ci)
-        x = problem.solve(x0=x0)
-        self.updateEtaTheta(x)
+        res = opt.minimize(fnc, x0, args, 'L-BFGS-B', fnc_der, bounds = bds, options={'disp': False})
+        if res.success:
+            return res.x
+        else:
+            print res.x
+            print res.status
+            print res.message
+            raise Exception('Minimizer did not converge')
 
-    def sampleWeighting(self):
+
+    def sampleWeighting(self, x, R, F):
         '''
         Compute the sample weighting for weighted ML update.
         '''
-        err = self.R - np.sum(self.theta.T * self.F, 1).reshape(-1, 1) # (N x 1)
-        return np.exp(err / self.eta) # (N x 1)
+        eta = x[0]
+        theta = x[1:]
+        err = R - np.sum(theta.T * F, 1).reshape(-1, 1) # (N x 1)
+        print 'Maximum err', np.max(err)
+        print 'Eta', eta
+        p = np.exp(err / eta)
+
+        #plt.plot(p)
+        #plt.show()
+        return p # (N x 1)
 
     def computeSampleWeighting(self, W, R, F, eps):
         '''
@@ -95,6 +140,41 @@ class DualFnc:
             F       Feature dataset matrix  (N x S)
             eps     Epsilon                 (1 x 1)
         '''
-        self.updateArguments(W, R, F, eps)
-        self.minimize()
-        return self.sampleWeighting()
+        x = self.minimize(W, R, F, eps)
+        return self.sampleWeighting(x, R, F)
+
+
+# def fnc(self, x, eval_grad = False):
+#     '''
+#     Implementation of the GPREPS dual function
+#
+#     Inputs:
+#         x           tuple containing (eta, theta)       (1 x 1+S)
+#         eval_grad   if True the graident is evaluated
+#
+#     Outputs:
+#         fval    value of the dual function at x         (1 x 1)
+#         grad    gradient of the dual function at x      (1 x 1+S)
+#     '''
+#     eta, theta = x[0], np.array(x[1:]).reshape(-1, 1)
+#
+#     # Evaluate dual function at x
+#     N = self.W.shape[0]
+#     mean_f = np.mean(self.F, 0).reshape(-1, 1)                  # (S x 1)
+#     err = self.R - np.sum(theta.T * self.F, 1).reshape(-1, 1)   # (N x 1)
+#     zeta = np.exp(err / float(eta))                             # (N x 1)
+#     s_zeta = np.sum(zeta)
+#     summa = s_zeta / float(N)
+#     fval = (eta * np.log(summa)) + (eta * self.eps) + (np.dot(theta.T, mean_f))
+#
+#     # Evaluate gradient
+#     if eval_grad:
+#         # Evaluate gradient w.r.t eta
+#         base = np.sum(zeta * err) / (eta * np.sum(zeta)) # (1 x 1)
+#         d_eta = self.eps + np.log(summa) - base
+#
+#         # Evaluate gradient w.r.t. theta
+#         d_theta = mean_f - (np.sum(zeta * self.F, 0).reshape(-1,1) / float(s_zeta)) # (S x 1)
+#         grad = np.append(d_eta, d_theta).reshape(1, -1) # (1 x S + 1)
+#         return fval, grad
+#     return fval
